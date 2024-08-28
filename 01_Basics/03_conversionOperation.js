d1678a080b15524a0949ce42260f0fe775d6f9cf
Abhay2003-harne <abhayharne8838@gmail.com>
2024-08-28 15:45:19 +0000

    conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -3,4 +3,8 @@ let score="25"
 console.log(typeof score)
 console.log(typeof (score))
 
-let valueInNumber
+let valueInNumber = Number(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
+
+
