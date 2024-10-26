5f985aa96fae2976811755e676be976decc055fd
Abhay2003-harne <abhayharne8838@gmail.com>
2024-10-26 14:22:20 +0000

    str

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,18 @@
+const playDemo = new String("Abhay") ;
+//console.log(playDemo[0])
+//console.log(playDemo[1])
+//console.log(playDemo[2])
+//console.log(playDemo[3])
+//console.log(playDemo[4])
+//console.log(playDemo)
+//console.log(playDemo.toLowerCase())
+//console.log(playDemo.charAt(1));
+
+//console.log(playDemo.slice(-8,4))
+//console.log(playDemo.slice(0,4))
+let newDemo=playDemo.slice(-8,4) 
+//console.log(newDemo)
+
+let demo2="   Abhay    "
+console.log(demo2)
+console.log(demo2.trim())
